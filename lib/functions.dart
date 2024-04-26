77ffd3cf2df0b081e718f4e5a5d125592cec391a
Malak <71153710+malak4822@users.noreply.github.com>
2024-04-26 21:23:00 +0200

    Update functions.dart

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,17 +1,22 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-/// A [ColorFunctions] containing functions related to color manipulation.
+/// A utility class containing functions for color manipulation.
 class ColorFunctions {
-  /// [changeColor] function sets and invoke [callBack] with new color param.
+  /// Changes the given color to a new random color and invokes the provided callback with the new color.
+  
+  /// The [color] parameter is the original color.
+  /// The [callBack] function is invoked with the new color.
   void changeColor(Color color, void Function(Color) callBack) {
-    // assigning random ints from 0 to 255 internally
+    // Generate random values for red, green, and blue components (0 to 255).
     final int redVal = Random().nextInt(256);
     final int greenVal = Random().nextInt(256);
     final int blueVal = Random().nextInt(256);
 
-    // assigning new randomly generated color to [randomColor] var
+    // Create a new color using the randomly generated RGB values and alpha value of 200.
     final Color randomColor = Color.fromARGB(200, redVal, greenVal, blueVal);
+
+    // Invoke the callback function with the new random color.
     callBack(randomColor);
   }
 }
